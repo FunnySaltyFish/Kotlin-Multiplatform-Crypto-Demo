7dcf42cdf6896140508cd531ca2ea76003ba2a70
FunnySaltyFish <3036548996@qq.com>
2023-03-27 19:02:40 +0800

    rename & optimize the implementation of hash(byteArray) in JVM

--- src/jvmMain/kotlin/com/funny/crypto/ShaFactories.kt
+++ src/jvmMain/kotlin/com/funny/crypto/ShaFactories.kt
@@ -10,8 +10,13 @@ actual object Sha256Factory {
              * @return
              */
             override fun hash(data: String): String {
-                val md = java.security.MessageDigest.getInstance("SHA-256")
-                return md.digest(data.toByteArray()).toHexString()
+                val sha256 = java.security.MessageDigest.getInstance("SHA-256")
+                return sha256.digest(data.toByteArray()).toHexString()
+            }
+
+            override fun hash(byteArray: ByteArray): String {
+                val sha256 = java.security.MessageDigest.getInstance("SHA-256")
+                return sha256.digest(byteArray).toHexString()
             }
         }
     }
@@ -27,8 +32,13 @@ actual object Sha512Factory {
              * @return
              */
             override fun hash(data: String): String {
-                val md = java.security.MessageDigest.getInstance("SHA-512")
-                return md.digest(data.toByteArray()).toHexString()
+                val sha512 = java.security.MessageDigest.getInstance("SHA-512")
+                return sha512.digest(data.toByteArray()).toHexString()
+            }
+
+            override fun hash(byteArray: ByteArray): String {
+                val sha512 = java.security.MessageDigest.getInstance("SHA-512")
+                return sha512.digest(byteArray).toHexString()
             }
         }
     }
